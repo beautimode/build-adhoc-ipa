ff90a00a34ff432b918fe68aa27f5bdce5ef0b53
Meng-Yuan Huang <myh@live.com>
2022-12-02 22:13:15 +0800

    * Support printing build logs. * Change build config to Release.

--- index.js
+++ index.js
@@ -22,6 +22,15 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -29,36 +38,69 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core = __importStar(require("@actions/core"));
 const child_process_1 = require("child_process");
 const fs_1 = __importDefault(require("fs"));
-try {
-    const xcworkspace = core.getInput('xcworkspace');
-    const schemes = JSON.parse(core.getInput('schemes'));
-    const manifestPlistImageUrl = core.getInput('manifestPlistImageUrl');
-    const manifestPlistIpaUrl = core.getInput('manifestPlistIpaUrl');
-    const manifestPlistTitle = core.getInput('manifestPlistTitle');
-    const manifestPlistBundleId = core.getInput('manifestPlistBundleId');
-    const manifestPlistBundleVersion = core.getInput('manifestPlistBundleVersion');
-    const manifestPlistTemplate = fs_1.default.readFileSync(__dirname + '/manifest.plist').toString();
-    const exportOptionsPlistTeamId = core.getInput('exportOptionsPlistTeamId');
-    const exportOptionsPlistTemplate = fs_1.default.readFileSync(__dirname + '/ExportOptions.plist').toString();
-    const exportOptionsPlist = exportOptionsPlistTemplate.replace('exportOptionsPlistTeamId', exportOptionsPlistTeamId);
-    fs_1.default.writeFileSync(`ExportOptions.plist`, exportOptionsPlist);
-    (0, child_process_1.execSync)(`mkdir -p ${manifestPlistBundleVersion}`);
-    schemes.forEach((s) => {
-        console.log(`${s}: run archiving...`);
-        (0, child_process_1.execSync)(`xcodebuild -workspace ${xcworkspace} -scheme ${s} -configuration Debug -sdk iphoneos -archivePath ${manifestPlistBundleVersion}/${s}.xcarchive -parallelizeTargets archive`, { stdio: 'ignore' });
-        const manifestPlist = manifestPlistTemplate.
-            replace('manifestPlistTitle', manifestPlistTitle).
-            replace('manifestPlistBundleId', manifestPlistBundleId).
-            replace('manifestPlistBundleVersion', manifestPlistBundleVersion).
-            replace('manifestPlistImageUrl', manifestPlistImageUrl).
-            replace('manifestPlistTitle', manifestPlistTitle).
-            replace('manifestPlistIpaUrl', `${manifestPlistIpaUrl}/${manifestPlistBundleVersion}/${s}.ipa`);
-        fs_1.default.writeFileSync(`${manifestPlistBundleVersion}/${s}-manifest.plist`, manifestPlist);
-        console.log(`${s}: run Ad Hoc IPA export...`);
-        (0, child_process_1.execSync)(`xcodebuild -parallelizeTargets -exportArchive -archivePath ${manifestPlistBundleVersion}/${s}.xcarchive -exportOptionsPlist ExportOptions.plist -exportPath ${s} -allowProvisioningUpdates`, { stdio: 'ignore' });
-        console.log(`${s}: exported successfully!`);
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            let res = '';
+            const xcworkspace = core.getInput('xcworkspace');
+            const schemes = JSON.parse(core.getInput('schemes'));
+            const manifestPlistImageUrl = core.getInput('manifestPlistImageUrl');
+            const manifestPlistIpaUrl = core.getInput('manifestPlistIpaUrl');
+            const manifestPlistTitle = core.getInput('manifestPlistTitle');
+            const manifestPlistBundleId = core.getInput('manifestPlistBundleId');
+            const manifestPlistBundleVersion = core.getInput('manifestPlistBundleVersion');
+            const manifestPlistTemplate = fs_1.default.readFileSync(__dirname + '/manifest.plist').toString();
+            const exportOptionsPlistTeamId = core.getInput('exportOptionsPlistTeamId');
+            const exportOptionsPlistTemplate = fs_1.default.readFileSync(__dirname + '/ExportOptions.plist').toString();
+            const exportOptionsPlist = exportOptionsPlistTemplate.replace('exportOptionsPlistTeamId', exportOptionsPlistTeamId);
+            fs_1.default.writeFileSync(`ExportOptions.plist`, exportOptionsPlist);
+            res = (0, child_process_1.execSync)(`mkdir -p ${manifestPlistBundleVersion}`).toString();
+            console.log(res);
+            for (let i = 0; i < schemes.length; i++) {
+                const s = schemes[i];
+                console.log(`${s}: run archiving...`);
+                yield spawnAsync(`xcodebuild -workspace ${xcworkspace} -scheme ${s} -configuration Release -sdk iphoneos -archivePath ${manifestPlistBundleVersion}/${s}.xcarchive -parallelizeTargets archive`);
+                const manifestPlist = manifestPlistTemplate.
+                    replace('manifestPlistTitle', manifestPlistTitle).
+                    replace('manifestPlistBundleId', manifestPlistBundleId).
+                    replace('manifestPlistBundleVersion', manifestPlistBundleVersion).
+                    replace('manifestPlistImageUrl', manifestPlistImageUrl).
+                    replace('manifestPlistTitle', manifestPlistTitle).
+                    replace('manifestPlistIpaUrl', `${manifestPlistIpaUrl}/${manifestPlistBundleVersion}/${s}.ipa`);
+                fs_1.default.writeFileSync(`${manifestPlistBundleVersion}/${s}-manifest.plist`, manifestPlist);
+                console.log(`${s}: run Ad Hoc IPA export...`);
+                yield spawnAsync(`xcodebuild -parallelizeTargets -exportArchive -archivePath ${manifestPlistBundleVersion}/${s}.xcarchive -exportOptionsPlist ExportOptions.plist -exportPath ${s} -allowProvisioningUpdates`);
+                console.log(`${s}: exported successfully!`);
+            }
+        }
+        catch (error) {
+            core.setFailed(error.message);
+        }
+        process.exit(0);
     });
 }
-catch (error) {
-    core.setFailed(error.message);
+function spawnAsync(cmdLine) {
+    return new Promise((resolve, reject) => {
+        const [cmd, ...args] = cmdLine.split(/\s+/);
+        const sp = (0, child_process_1.spawn)(cmd, args);
+        sp.on('message', console.log);
+        let data = '';
+        sp.stdout.on('data', chunk => {
+            data += chunk.toString();
+        });
+        let errData = '';
+        sp.stderr.on('data', chunk => {
+            errData += chunk.toString();
+        });
+        sp.on('close', code => {
+            if ((code || 0) > 0) {
+                return reject(new Error(`${cmdLine} error:\n${errData}`));
+            }
+            resolve(data);
+        });
+        sp.on('error', err => {
+            reject(err);
+        });
+    });
 }
+main();
--- index.ts
+++ index.ts
@@ -1,42 +1,82 @@
 import * as core from '@actions/core';
-import { execSync } from 'child_process';
+import { execSync, spawn } from 'child_process';
 import fs from 'fs';
 
-try {
-  const xcworkspace = core.getInput('xcworkspace');
-  const schemes = JSON.parse(core.getInput('schemes')) as string[];
-  const manifestPlistImageUrl = core.getInput('manifestPlistImageUrl');
-  const manifestPlistIpaUrl = core.getInput('manifestPlistIpaUrl');
-  const manifestPlistTitle = core.getInput('manifestPlistTitle');
-  const manifestPlistBundleId = core.getInput('manifestPlistBundleId');
-  const manifestPlistBundleVersion = core.getInput('manifestPlistBundleVersion');
-  const manifestPlistTemplate = fs.readFileSync(__dirname + '/manifest.plist').toString();
+async function main() {
+  try {
+    let res = '';
+    const xcworkspace = core.getInput('xcworkspace');
+    const schemes = JSON.parse(core.getInput('schemes')) as string[];
+    const manifestPlistImageUrl = core.getInput('manifestPlistImageUrl');
+    const manifestPlistIpaUrl = core.getInput('manifestPlistIpaUrl');
+    const manifestPlistTitle = core.getInput('manifestPlistTitle');
+    const manifestPlistBundleId = core.getInput('manifestPlistBundleId');
+    const manifestPlistBundleVersion = core.getInput('manifestPlistBundleVersion');
+    const manifestPlistTemplate = fs.readFileSync(__dirname + '/manifest.plist').toString();
 
-  const exportOptionsPlistTeamId = core.getInput('exportOptionsPlistTeamId');
-  const exportOptionsPlistTemplate = fs.readFileSync(__dirname +'/ExportOptions.plist').toString();
+    const exportOptionsPlistTeamId = core.getInput('exportOptionsPlistTeamId');
+    const exportOptionsPlistTemplate = fs.readFileSync(__dirname + '/ExportOptions.plist').toString();
 
-  const exportOptionsPlist = exportOptionsPlistTemplate.replace('exportOptionsPlistTeamId', exportOptionsPlistTeamId);
-  fs.writeFileSync(`ExportOptions.plist`, exportOptionsPlist);
+    const exportOptionsPlist = exportOptionsPlistTemplate.replace('exportOptionsPlistTeamId', exportOptionsPlistTeamId);
+    fs.writeFileSync(`ExportOptions.plist`, exportOptionsPlist);
 
-  execSync(`mkdir -p ${manifestPlistBundleVersion}`);
-  schemes.forEach((s) => {
-    console.log(`${s}: run archiving...`);
-    execSync(`xcodebuild -workspace ${xcworkspace} -scheme ${s} -configuration Debug -sdk iphoneos -archivePath ${manifestPlistBundleVersion}/${s}.xcarchive -parallelizeTargets archive`, { stdio: 'ignore' });
+    res = execSync(`mkdir -p ${manifestPlistBundleVersion}`).toString();
+    console.log(res);
+    for (let i = 0; i < schemes.length; i++) {
+      const s = schemes[i];
 
-    const manifestPlist = manifestPlistTemplate.
-    replace('manifestPlistTitle', manifestPlistTitle).
-    replace('manifestPlistBundleId', manifestPlistBundleId).
-    replace('manifestPlistBundleVersion', manifestPlistBundleVersion).
-    replace('manifestPlistImageUrl', manifestPlistImageUrl).
-    replace('manifestPlistTitle', manifestPlistTitle).
-    replace('manifestPlistIpaUrl', `${manifestPlistIpaUrl}/${manifestPlistBundleVersion}/${s}.ipa`);
-    fs.writeFileSync(`${manifestPlistBundleVersion}/${s}-manifest.plist`, manifestPlist);
+      console.log(`${s}: run archiving...`);
+      await spawnAsync(`xcodebuild -workspace ${xcworkspace} -scheme ${s} -configuration Release -sdk iphoneos -archivePath ${manifestPlistBundleVersion}/${s}.xcarchive -parallelizeTargets archive`);
 
-    console.log(`${s}: run Ad Hoc IPA export...`);
-    execSync(`xcodebuild -parallelizeTargets -exportArchive -archivePath ${manifestPlistBundleVersion}/${s}.xcarchive -exportOptionsPlist ExportOptions.plist -exportPath ${s} -allowProvisioningUpdates`, { stdio: 'ignore' });
+      const manifestPlist = manifestPlistTemplate.
+        replace('manifestPlistTitle', manifestPlistTitle).
+        replace('manifestPlistBundleId', manifestPlistBundleId).
+        replace('manifestPlistBundleVersion', manifestPlistBundleVersion).
+        replace('manifestPlistImageUrl', manifestPlistImageUrl).
+        replace('manifestPlistTitle', manifestPlistTitle).
+        replace('manifestPlistIpaUrl', `${manifestPlistIpaUrl}/${manifestPlistBundleVersion}/${s}.ipa`);
+      fs.writeFileSync(`${manifestPlistBundleVersion}/${s}-manifest.plist`, manifestPlist);
 
-    console.log(`${s}: exported successfully!`);
-  });
-} catch (error: any) {
-  core.setFailed(error.message);
+      console.log(`${s}: run Ad Hoc IPA export...`);
+      await spawnAsync(`xcodebuild -parallelizeTargets -exportArchive -archivePath ${manifestPlistBundleVersion}/${s}.xcarchive -exportOptionsPlist ExportOptions.plist -exportPath ${s} -allowProvisioningUpdates`);
+
+      console.log(`${s}: exported successfully!`);
+    }
+  } catch (error: any) {
+    core.setFailed(error.message);
+  }
+  process.exit(0);
 }
+
+function spawnAsync(cmdLine: String) {
+  return new Promise((resolve, reject) => {
+    const [cmd, ...args] = cmdLine.split(/\s+/);
+    const sp = spawn(cmd, args);
+
+    sp.on('message', console.log);
+
+    let data = '';
+    sp.stdout.on('data', chunk => {
+      data += chunk.toString();
+    });
+
+    let errData = '';
+    sp.stderr.on('data', chunk => {
+      errData += chunk.toString();
+    });
+
+    sp.on('close', code => {
+      if ((code || 0) > 0) {
+        return reject(new Error(`${cmdLine} error:\n${errData}`));
+      }
+
+      resolve(data);
+    });
+
+    sp.on('error', err => {
+      reject(err);
+    });
+  });
+}
+
+main();
