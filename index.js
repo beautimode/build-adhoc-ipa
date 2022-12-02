869e9e3fe392873e2bffb4e0317e631b7c43709f
Meng-Yuan Huang <myh@live.com>
2022-12-02 13:53:33 +0800

    * Fix bugs.

--- index.js
+++ index.js
@@ -1,21 +1,44 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const core_1 = __importDefault(require("@actions/core"));
+const core = __importStar(require("@actions/core"));
 const child_process_1 = require("child_process");
 const fs_1 = __importDefault(require("fs"));
 try {
-    const xcworkspace = core_1.default.getInput('xcworkspace');
-    const schemes = JSON.parse(core_1.default.getInput('schemes'));
-    const manifestPlistImageUrl = core_1.default.getInput('manifestPlistImageUrl');
-    const manifestPlistIpaUrl = core_1.default.getInput('manifestPlistIpaUrl');
-    const manifestPlistTitle = core_1.default.getInput('manifestPlistTitle');
-    const manifestPlistBundleId = core_1.default.getInput('manifestPlistBundleId');
-    const manifestPlistBundleVersion = core_1.default.getInput('manifestPlistBundleVersion');
+    const xcworkspace = core.getInput('xcworkspace');
+    const schemes = JSON.parse(core.getInput('schemes'));
+    const manifestPlistImageUrl = core.getInput('manifestPlistImageUrl');
+    const manifestPlistIpaUrl = core.getInput('manifestPlistIpaUrl');
+    const manifestPlistTitle = core.getInput('manifestPlistTitle');
+    const manifestPlistBundleId = core.getInput('manifestPlistBundleId');
+    const manifestPlistBundleVersion = core.getInput('manifestPlistBundleVersion');
     const manifestPlistTemplate = fs_1.default.readFileSync('manifest.plist').toString();
-    const exportOptionsPlistTeamId = core_1.default.getInput('exportOptionsPlistTeamId');
+    const exportOptionsPlistTeamId = core.getInput('exportOptionsPlistTeamId');
     const exportOptionsPlistTemplate = fs_1.default.readFileSync('ExportOptions.plist').toString();
     const exportOptionsPlist = exportOptionsPlistTemplate.replace('exportOptionsPlistTeamId', exportOptionsPlistTeamId);
     fs_1.default.writeFileSync(`ExportOptions.plist`, exportOptionsPlist);
@@ -37,5 +60,5 @@ try {
     });
 }
 catch (error) {
-    core_1.default.setFailed(error.message);
+    core.setFailed(error.message);
 }
--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import core from '@actions/core';
+import * as core from '@actions/core';
 import { execSync } from 'child_process';
 import fs from 'fs';
 
